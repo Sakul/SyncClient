7c9e6e7a91c983a0c3d072b6d849e5605e5d242f
Sakul <au.perspectives@gmail.com>
2021-05-27 21:45:08 +0700

    Create simple projects.

---
+++ Demo/SyncClient.DemoApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SyncClient.DemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SyncClient.Core/Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SyncClient.Core
+{
+    public class Class1
+    {
+    }
+}
