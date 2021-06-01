e7656763498d5b204e820226130c07145ed797eb
Sakul <au.perspectives@gmail.com>
2021-06-02 06:59:09 +0700

    Setup app configuration file.

---
+++ SyncClient.Core/Services/SocketSyncServices/Models/SocketSyncOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace SyncClient.Services.SocketSyncServices.Models
+{
+    public class SocketSyncOptions
+    {
+        public string HostUrl { get; set; }
+        public int Port { get; set; }
+    }
+}
